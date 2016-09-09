b35073f9e878b6d901a62a964fdc7989213e60a6
Jennyf19 <ferriesj@gmail.com>
2016-09-08 19:08:40 -0700

    unit tests

---
+++ binaryTreePractice/BinaryTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binaryTreePractice
+{
+    class BinaryTree
+    {
+        internal int Size()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal int Insert()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal int Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal int Find()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal int nodeTrack()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ binaryTreePractice/BinaryTreeTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace binaryTreePractice
+{
+    public class BinaryTreeTest
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            var treeSize = new BinaryTree();
+
+            Assert.Equal(treeSize.Size(), 0);
+
+        }
+
+        [Fact]
+        public void TestMethod2()
+        {
+            var treeInsert = new BinaryTree();
+
+            Assert.Equal(treeInsert.GetHashCode(), 0);
+        }
+
+        [Fact]
+        public void TestMethod3()
+        {
+            var treeDelete = new BinaryTree();
+
+            Assert.Equal(treeDelete.GetHashCode(), 0);
+        }
+
+        [Fact]
+        public void TestMethod4()
+        {
+            var treeFind = new BinaryTree();
+
+            Assert.Equal(treeFind.GetHashCode(), 0);
+        }
+
+        [Fact]
+        public void TestMethod5()
+        {
+            var nodeTrack = new BinaryTree();
+
+            Assert.Equal(nodeTrack.GetHashCode(), 0);
+        }
+    }
+}
